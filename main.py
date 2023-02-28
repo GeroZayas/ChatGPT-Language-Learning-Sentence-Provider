afc98abf77592ac11ddfcc06ce0f9183f84ad177
GeroZayas <gerozayas@gmail.com>
2023-02-28 15:56:44 +0100

    Update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 from rich import print
+import threading
 
 
 from dotenv import load_dotenv
@@ -19,15 +20,22 @@ openai.api_key = OPENAI_API_KEY
 
 
 # ----------------------------------------------------------------
-def waiting_for_answer_animation():
+def waiting_for_answer_animation(stop_event):
     """This function prints a message for the user to know that the
     answer is coming and the program is running. It uses time.sleep() to
     create a simple animation of every character in the string.
     """
-    wait_string = "Your answer is coming..."
-    for character in wait_string:
-        print(character, end="")
-        time.sleep(0.1)
+    counter = 0
+    wait_string = "."
+    while not stop_event.is_set():
+        for char in wait_string:
+            if counter == 79:
+                counter = 0
+                print("\n")
+            print(char, end="")
+            counter += 1
+            time.sleep(0.1)
+        time.sleep(0.01)
 
 
 def generate_response(prompt):
@@ -74,13 +82,43 @@ while program_run:
     prompt = input("Insert word or phrase: \n>>> ")
     prompt += f"Act as if you were an amazing teacher of {lang} and you are teaching me this language and give me {num_of_phrases} long sentences  in {lang} with this word or phrase: '{prompt}' so I can learn it very well"
 
-    waiting_for_answer_animation()
+    # waiting_for_answer_animation()
 
-    response = generate_response(prompt)
+    # response = generate_response(prompt)
 
-    print()
+    def response_func(stop_event):
+        try:
+            response = generate_response(prompt)
+            print()
+            print(f"[bold yellow]{response}[/bold yellow]")
+            print()
+            stop_event.set()
+        except Exception:
+            print("Something went wrong!")
 
-    print(f"[bold yellow]{response}[/bold yellow]")
+    # ----------------------------------------------------------------
+    ###########
+    # THREADS #
+    ###########
+
+    # Create a shared event to signal the first thread to stop
+    stop_event = threading.Event()
+
+    # Create threads for each function
+    thread1 = threading.Thread(target=waiting_for_answer_animation, args=(stop_event,))
+    thread2 = threading.Thread(target=response_func, args=(stop_event,))
+
+    # Start both threads
+    thread1.start()
+    thread2.start()
+
+    # Wait for both threads to finish before exiting
+    thread1.join()
+    thread2.join()
+    # ----------------------------------------------------------------
+    # response_func()
+
+    # print(f"[bold yellow]{response}[/bold yellow]")
 
     print("-" * 60)
 
